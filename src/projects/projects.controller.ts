5a8afef5188a384743a65a0e5aa10ab92aaad26e
Sushovan Majhi <sush@smajhi.com>
2021-01-29 20:46:39 -0600

    add upload

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Param, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import * as dt from 'directory-tree';
+import { diskStorage } from 'multer';
 import * as fs from 'fs';
 
 @Controller('projects')
@@ -9,13 +10,30 @@ export class ProjectsController {
     @Get(':id/index')
     getIndex(@Param('id') id) {
         const dname = '/tmp/projects/' + id;
-        const index = dt(dname, { exclude: /.cotex|.git|node_modules/, attributes: ['mtime'] });
-        return index;
+        const res = {};
+        dt(dname, { exclude: /.cotex|.git|node_modules/, attributes: ['mtime'] }, (file, path, stats) => {
+            res[file.path.split('/').slice(3).join('/')] = file;
+        });
+        return res;
     }
 
     @Post(':id/sync')
-    @UseInterceptors(FileInterceptor('file'))
+    @UseInterceptors(FileInterceptor('file', {
+        storage: diskStorage({
+            destination: (req, file, cb) => {
+                const dir = '/tmp/projects/' + req.params.id + '/' + req.body.path;
+                
+                if(!fs.existsSync(dir)) {
+                    fs.mkdirSync(dir, { recursive: true });
+                }
+                return cb(null,dir);
+            },
+            filename: (req, file, cb) => {
+                return cb(null, file.originalname);
+            }
+        })
+    }))
     uploadFile(@UploadedFile() file) {
-        console.log(file);
+       //
     }
 }
